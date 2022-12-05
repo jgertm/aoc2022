601aea1b5efc5a9cf6e125f755fb84d9da3eb858
Tim Jäger <jger.tm@gmail.com>
2022-12-04 21:53:55 -0800

    day5 part2

--- src/aoc2022/day5.clj
+++ src/aoc2022/day5.clj
@@ -118,14 +118,14 @@ move 1 from 1 to 2")
            (move initial {:count 1 :from 2 :to 1})))))
 
 (defn rearrange
-  [{:keys [initial moves]}]
+  [move {:keys [initial moves]}]
   (reduce move initial moves))
 
 (deftest rearrange-test
   (is (= {1 ["C"]
           2 ["M"]
           3 ["P" "D" "N" "Z"]}
-         (rearrange (parse sample)))))
+         (rearrange move (parse sample)))))
 
 (defn tops
   [stacks]
@@ -141,7 +141,7 @@ move 1 from 1 to 2")
   [x]
   (->> x
       parse
-      rearrange
+      (rearrange move)
       tops
       (apply str)))
 
@@ -154,5 +154,86 @@ move 1 from 1 to 2")
 
 (comment
   (solve-part1 input)
+
+  )
+
+;; --- Part Two ---
+
+;; As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+;; Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+;; The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+;; Again considering the example above, the crates begin in the same configuration:
+
+;;     [D]
+;; [N] [C]
+;; [Z] [M] [P]
+;;  1   2   3
+
+;; Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+;; [D]
+;; [N] [C]
+;; [Z] [M] [P]
+;;  1   2   3
+
+;; However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+;;         [D]
+;;         [N]
+;;     [C] [Z]
+;;     [M] [P]
+;;  1   2   3
+
+;; Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+;;         [D]
+;;         [N]
+;; [C]     [Z]
+;; [M]     [P]
+;;  1   2   3
+
+;; Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+;;         [D]
+;;         [N]
+;;         [Z]
+;; [M] [C] [P]
+;;  1   2   3
+
+;; In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+;; Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+(defn move-preserving
+  [stacks {:keys [count from to]}]
+  (let [moved (take-last count (get stacks from))]
+    (-> stacks
+        (update from #(->> % (drop-last count) vec))
+        (update to (fnil into []) moved))))
+
+(deftest move-preserving-test
+  (let [{:keys [initial]} (parse sample)]
+    (is (= {1 ["Z" "N"]
+            2 ["M"]
+            3 ["P" "C" "D"]}
+           (move-preserving initial {:count 2 :from 2 :to 3})))))
+
+(defn solve-part2
+  [x]
+  (->> x
+       parse
+       (rearrange move-preserving)
+       tops
+       (apply str)))
+
+(deftest solve-part2-test
+  (is (= "MCD"
+         (solve-part2 sample))))
+
+(comment
+  (solve-part2 input)
 
   )
