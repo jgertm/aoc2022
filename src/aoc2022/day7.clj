98b800dcf6eb374839e75eec38facb2894cd9897
Tim Jäger <jger.tm@gmail.com>
2022-12-06 21:49:57 -0800

    day7 part2

--- src/aoc2022/day7.clj
+++ src/aoc2022/day7.clj
@@ -202,3 +202,52 @@ $ ls
   (solve-part1 input)
 
   )
+
+;; --- Part Two ---
+
+;; Now, you're ready to choose a directory to delete.
+
+;; The total disk space available to the filesystem is 70000000. To run the update, you need unused space of at least 30000000. You need to find a directory you can delete that will free up enough space to run the update.
+
+;; In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; this means that the size of the unused space must currently be 21618835, which isn't quite the 30000000 required by the update. Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+
+;; To achieve this, you have the following options:
+
+;;     Delete directory e, which would increase unused space by 584.
+;;     Delete directory a, which would increase unused space by 94853.
+;;     Delete directory d, which would increase unused space by 24933642.
+;;     Delete directory /, which would increase unused space by 48381165.
+
+;; Directories e and a are both too small; deleting them would not free up enough space. However, directories d and / are both big enough! Between these, choose the smallest: d, increasing unused space by 24933642.
+
+;; Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. What is the total size of that directory?
+
+(def disk-size 70000000)
+(def update-size 30000000)
+
+(defn select
+  [x]
+  (let [used (:total (first x))
+        min-size (- update-size (- disk-size used))]
+    (->> x
+         (sort-by :total)
+         (some #(and (>= (:total %) min-size) %)))))
+
+(defn solve-part2
+  [x]
+  (-> x
+      parse
+      layout
+      accumulate
+      flatten
+      select
+      :total))
+
+(deftest solve-part2-test
+  (is (= 24933642
+         (solve-part2 sample))))
+
+(comment
+  (solve-part2 input)
+
+  )
